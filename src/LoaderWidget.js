3eb6c0a8b4b4889c974aed0f782426968f8f484d
liaozhai <marunov@scanex.ru>
2018-01-31 16:49:48 +0300

    updated

--- src/LoaderWidget.js
+++ src/LoaderWidget.js
@@ -1,12 +1,18 @@
 import './LoaderWidget.css';
+import { Translations } from 'lib/Translations/src/Translations.js';
+import { EventTarget } from 'lib/EventTarget/src/EventTarget.js';
 
-class LoaderWidget {
+window.Catalog.translations = window.Catalog.translations || new Translations();
+let T = window.Catalog.translations;
+
+class LoaderWidget extends EventTarget {
     constructor () {
+        super();
         this._container = document.createElement('div');
         document.body.appendChild(this._container);
         this._container.classList.add ('noselect');
         this._container.classList.add('loader-widget');
-        this._container.innerHTML = '<div class="loader-icon"></div>';
+        this._container.innerHTML = `<div class="loader-icon"></div><div class="loader-button">${T.getText('alerts.cancel')}</div>`;
         this._ovl = document.querySelector('.loader-widget-overlay');
         this._stopPropagation = this._stopPropagation.bind(this);
         if(!this._ovl) {
@@ -18,6 +24,13 @@ class LoaderWidget {
             this._ovl.addEventListener('mousewheel', this._stopPropagation);
             this._ovl.addEventListener('click', this._stopPropagation);
         }
+        this._container.querySelector('.loader-button').addEventListener('click', e => {
+            this.hide();
+            
+            let event = document.createEvent('Event');
+            event.initEvent('cancel', false, false);
+            this.dispatchEvent(event);
+        });
         this.show = this.show.bind(this);
         this.hide = this.hide.bind(this);
     }
